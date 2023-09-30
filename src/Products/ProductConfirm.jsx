cf1bdbed92e0d56dfe3bf19d89cc22c91cb5aa89
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-10-01 00:34:35 +0700

    Update ProductConfirm.jsx

--- src/Products/ProductConfirm.jsx
+++ src/Products/ProductConfirm.jsx
@@ -2,6 +2,7 @@ import "./css/Product.css";
 import { MainButton } from "@twa-dev/sdk/react" 
 import { useState } from "react"
 import { useParams, useLocation } from "react-router-dom";
+import Stories from "../Stories/Stories"
 
 function ProductConfirm() {
   const { productId, size, price, name, img } = useParams();
@@ -19,26 +20,29 @@ function ProductConfirm() {
   );
   return (
     <>
-    <div className="full-item" key={productId}>
+    <div className="confirm-item" key={productId}>
     <img src={"/img/img/" + productData.img} alt="photo" />
     
-    <div className="full-item-name">{productData.name}
+    <div className="confirm-item-name">{productData.name}
     
-        
-      <div className="full-item-size">
-        <b> размер {size} US</b>
-      </div>
+        <span className="confirm-item-size" > размер {size} US</span>
    
       </div>
     
-      <div className="full-item-price">
-         <b>{price}₽</b>
+      <div className="confirm-item-price">
+         {price}₽
       </div>
       <div className="public-oferta">
         <p className="public-ofert-text">Оплачивая заказ, вы соглашаетесь <br/>с условиями <a className="public-oferta-link">публичной оферты</a></p>
       </div>
-      
+     
     
+  </div>
+  <div className="help-ful">
+    <h2 className="help-title">Полезная инофрмация</h2>
+    <div className="help-stories">
+    <Stories />
+    </div>
   </div>
   <MainButton 
         onClick={() => {
