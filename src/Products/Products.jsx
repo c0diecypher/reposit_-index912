b1051b4e6cf01261c3390e4cff4f4388d081bb24
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-10-02 15:27:12 +0700

    Update Products.jsx

--- src/Products/Products.jsx
+++ src/Products/Products.jsx
@@ -5,14 +5,14 @@ import { Link } from "react-router-dom";
 const Products = () => {
   const products = productsData.map((product) => (
     <div className="item" key={product.id}> {/* Перенесли ключ сюда */}
+        <Link to={`/products/${product.id}`}>
       <img src={"/img/img/" + product.img} alt="" />
       <h4>{product.price}₽</h4>
       <p>{product.name}</p>
       <button className="add-item">
-        <Link to={`/products/${product.id}`}>
           <div className="buy-item">Купить</div>
-        </Link>
       </button>
+        </Link>
     </div>
   ));
 
