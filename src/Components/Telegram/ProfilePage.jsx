8f7254abba437a234e0953eb63af20c234181690
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-10-02 15:03:39 +0700

    Create ProfilePage.jsx

---
+++ src/Components/Telegram/ProfilePage.jsx
@@ -0,0 +1,35 @@
+
+import '../../css/body.css'
+import './css/ProfilePage.css'
+import { useTelegram } from "../Hooks/useTelegram";
+import { InitialsAvatar } from "@twa-dev/mark42";
+
+function ProfilePage() {
+  const { user } = useTelegram();
+
+  return (
+    <>
+        <div className="profile-header">
+            <div className="profile-avatar-box">
+                <div className="profile-avatar-transparent">
+                    <div className="profile-avatar">
+                    <InitialsAvatar
+                        className="profile-avatar"
+                        userName={user?.first_name}
+                        entityId={2}
+                        entityName={`${user?.first_name}`}
+                        size={100}
+                        theme="apple"
+                        
+        />
+                    </div>
+             <div className="profile-name">{user?.first_name}</div>
+                </div>
+            </div>
+            
+        </div>
+    </>
+  )
+}
+
+export default ProfilePage;
