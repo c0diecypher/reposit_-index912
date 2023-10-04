17d55264bed776cc044309809351d001bf89358d
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-10-04 16:59:28 +0700

    Update ProductConfirm.jsx

--- src/Products/ProductConfirm.jsx
+++ src/Products/ProductConfirm.jsx
@@ -31,7 +31,7 @@ function ProductConfirm() {
       queryId
     };
 
-    fetch('http://45.12.228.26:8000/web-data', {
+    fetch('http://81.31.247.236:8000/web-data', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
