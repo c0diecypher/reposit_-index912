390b165cafcf79e0cc8c340f36932d5e64a9743a
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-10-07 20:16:14 +0700

    Update ProfilePage.jsx

--- src/Components/Telegram/ProfilePage.jsx
+++ src/Components/Telegram/ProfilePage.jsx
@@ -3,13 +3,47 @@ import '../../css/body.css'
 import './css/ProfilePage.css'
 import { useTelegram } from "../Hooks/useTelegram";
 import { InitialsAvatar } from "@twa-dev/mark42";
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function ProfilePage() {
+  const { user } = useTelegram();
   useEffect(() => {
     window.scrollTo(0, 0); // Прокрутка вверх при загрузке страницы
   }, []);
-  const { user } = useTelegram();
+  const [fullName, setFullName] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [savedData, setSavedData] = useState(null);
+
+  // Функция для сохранения данных в облаке
+  const saveDataToCloud = () => {
+    if (fullName && phoneNumber) {
+      window.Telegram.CloudStorage.setItem('user_data', JSON.stringify({ fullName, phoneNumber }), (error, success) => {
+        if (success) {
+          alert('Данные успешно сохранены в облаке Telegram.');
+        } else {
+          alert('Произошла ошибка при сохранении данных.');
+        }
+      });
+    } else {
+      alert('Пожалуйста, заполните ФИО и номер телефона.');
+    }
+  };
+
+  // Функция для получения данных из облака
+  const getDataFromCloud = () => {
+    window.Telegram.CloudStorage.getItem('user_data', (error, data) => {
+      if (data) {
+        setSavedData(JSON.parse(data));
+      } else {
+        alert('Данные не найдены в облаке Telegram.');
+      }
+    });
+  };
+
+  useEffect(() => {
+    getDataFromCloud();
+  }, []);
+
 
   return (
     <>
@@ -27,11 +61,39 @@ function ProfilePage() {
                         
         />
                     </div>
-             <div className="profile-name">{user?.first_name}</div>
-                </div>
-            </div>
-            
-        </div>
+             <div className="profile-name">{user?.first_name}Admin</div>
+                                    </div>
+                                </div>
+                            </div>
+                                <div className="profile-data">
+                                <div>
+                          <h1>Форма для сохранения данных в облаке Telegram</h1>
+                          <div>
+                            <label>ФИО:</label>
+                            <input
+                              type="text"
+                              value={fullName}
+                              onChange={(e) => setFullName(e.target.value)}
+                            />
+                          </div>
+                          <div>
+                            <label>Номер телефона:</label>
+                            <input
+                              type="text"
+                              value={phoneNumber}
+                              onChange={(e) => setPhoneNumber(e.target.value)}
+                            />
+                          </div>
+                          <button onClick={saveDataToCloud}>Сохранить в облаке</button>
+                          {savedData && (
+                            <div>
+                              <h2>Сохраненные данные:</h2>
+                              <p>ФИО: {savedData.fullName}</p>
+                              <p>Номер телефона: {savedData.phoneNumber}</p>
+                            </div>
+                          )}
+                        </div>
+             </div>
     </>
   )
 }
