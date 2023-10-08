6f716534df3404018c3d6e3218268547dbe3dbf4
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-10-08 12:56:25 +0700

    Update ProfilePage.jsx

--- src/Components/Telegram/ProfilePage.jsx
+++ src/Components/Telegram/ProfilePage.jsx
@@ -71,7 +71,7 @@ function ProfilePage() {
           </span>
         )}
       </p>
-              {renderPhoneNumberStatus()}
+              {renderPhoneNumberStatus}
               <CloudStorage />
                 
                     
