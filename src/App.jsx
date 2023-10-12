c7c9dd86f1e108435c4ba1b9f57f41884eb6e433
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-10-12 15:54:40 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -94,8 +94,7 @@ function App() {
     headers: {
       'Authorization': `twa-init-data ${initData}`,
       'Content-Type': 'application/json',
-      },
-    body: JSON.stringify({ hash, auth_date, user, query_id }),
+      }
   })
     .then(response => response.json())
     .then(data => {
