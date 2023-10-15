fdf4e42698a50ce2d1748385360e35405d58d4db
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-10-15 17:59:26 +0700

    Update productsData.jsx

--- src/Products/productsData.jsx
+++ src/Products/productsData.jsx
@@ -36,91 +36,189 @@ export default [
     }
   },
   {
-    id: "2",
-    name: "Nike AirForce",
-    img: "nike-airforce-midnightNavy.jpg",
-    category: "Nike",
-    description:
-      "He bends! He stretches! He even ties in knots, but always returns to his original shape!",
-    price: "5 300",
-    size: {
-      "7": "9 999",
-      "8": "17 499",
-      "9": "5 300",
-      "10": "10 499"
+    "id": "2",
+    "name": "Nike Air Force 1 Low 07 White",
+    "img": [
+      "https://cdn-img.poizonapp.com/pro-img/origin-img/20220212/ead653b6adf34cf2954b2d82cd495aae.jpg",
+      "https://cdn-img.poizonapp.com/pro-img/origin-img/20220212/9748e7d5afcb44c3a45dbad820d639f1.jpg",
+      "https://cdn-img.poizonapp.com/pro-img/origin-img/20220212/24999091120f4a839c7529ef87bd4eac.jpg",
+      "https://cdn-img.poizonapp.com/pro-img/origin-img/20220212/99f19c1605f74d46aa4405665785db47.jpg",
+      "https://cdn-img.poizonapp.com/pro-img/origin-img/20220212/64f362a7ed4f41d19f46d321e6b8a87d.jpg"
+    ],
+    "category": "Nike",
+    "price": "$--",
+    "size": {
+      "6": "$132",
+      "6.5": "$126",
+      "7": "$125",
+      "7.5": "$124",
+      "8": "$112",
+      "8.5": "$114",
+      "9": "$111",
+      "9.5": "$113",
+      "10": "$111",
+      "10.5": "$110",
+      "11": "$112",
+      "11.5": "$154",
+      "12": "$154",
+      "12.5": "$202",
+      "13": "$140",
+      "14": "$173",
+      "15": "$258"
     }
   },
   {
-    id: "3",
-    name: "Vans OldSkool",
-    img: "Vans-oldSkool.jpg",
-    category: "Vans",
-    description:
-      "It's a race, it's a chase, hurry up and feed their face! Who will win? No one knows! Feed the hungry hip-ip-pos!",
-    price: "8 590",
-    size: {
-      "8.5": "12 499",
-      "9": "12 199",
-      "9.5": "8 590",
-      "10": "10 499"
+    "id": "3",
+    "name": "Nike Dunk Low Retro White Black Panda (2021) (W)",
+    "img": [
+      "https://cdn-img.poizonapp.com/pro-img/origin-img/20230721/cc3d42c12794417891cd04316e028a55.jpg",
+      "https://cdn-img.poizonapp.com/pro-img/origin-img/20230721/c6499cad674b411eb37c7cc1f68d4114.jpg",
+      "https://cdn-img.poizonapp.com/pro-img/origin-img/20230721/36a2899822c1400a9ebcb8476b530f5e.jpg",
+      "https://cdn-img.poizonapp.com/pro-img/origin-img/20230721/1e2217fbb2c544c0ae1f4199505fb2bb.jpg",
+      "https://cdn-img.poizonapp.com/pro-img/origin-img/20230721/2a8a70fb06894e22b0c0847b976b1a5a.jpg"
+    ],
+    "category": "Nike",
+    "price": "$--",
+    "size": {
+      "5": "$130",
+      "5.5": "$129",
+      "6": "$127",
+      "6.5": "$126",
+      "7": "$123",
+      "7.5": "$123",
+      "8": "$130",
+      "8.5": "$131",
+      "9": "$135",
+      "9.5": "$133",
+      "10": "$133",
+      "10.5": "$178",
+      "11": "$176",
+      "11.5": "$176",
+      "12": "$201"
     }
   },
   {
-    id: "4",
-    name: "Converse Chuck Taylor ",
-    img: "Converse-ChuckTaylorAsCore.jpg",
-    category: "Converse",
-    description: "You'll get caught up in the crossfire!",
-    price: "9 899",
-    size: {
-      "6": "9 999",
-      "6.5": "12 000",
-      "7": "13 499",
-      "7.5": "16 499",
-      "8": "17 499",
-      "8.5": "12 499",
-      "9": "9 899",
-      "9.5": "11 499",
-      "10": "10 499"
+    "id": "4",
+    "name": "Nike Dunk Low Retro White Black Panda (2021) (GS)",
+    "img": [
+      "https://cdn-img.poizonapp.com/pro-img/origin-img/20230721/3fa438f3719c4e26b54a38b4bdc7e391.jpg",
+      "https://cdn-img.poizonapp.com/pro-img/origin-img/20230721/a6d5a14caeac44aebe947a7730650866.jpg",
+      "https://cdn-img.poizonapp.com/pro-img/origin-img/20230721/50da5cf22b3d4801aa40b1439aedd45a.jpg",
+      "https://cdn-img.poizonapp.com/pro-img/origin-img/20230721/f95d84ca3f8540cdbea6c18ad2123c73.jpg",
+      "https://cdn-img.poizonapp.com/pro-img/origin-img/20230721/981365ab6ff64e829be0aa3b07732b66.jpg"
+    ],
+    "category": "Nike",
+    "price": "$111",
+    "size": {
+      "3.5Y": "$112",
+      "4Y": "$110",
+      "4.5Y": "$117",
+      "5Y": "$116",
+      "5.5Y": "$117",
+      "6Y": "$110",
+      "6.5Y": "$117",
+      "7Y": "$111"
     }
   },
   {
-    id: "5",
-    name: "PUMA RS-X",
-    img: "Puma-Rs-X.jpg",
-    category: "Puma",
-    description:
-      "It's a race, it's a chase, hurry up and feed their face! Who will win? No one knows! Feed the hungry hip-ip-pos!",
-    price: "15 999",
-    size: {
-      "6": "9 999",
-      "6.5": "12 000",
-      "7": "13 499",
-      "7.5": "16 499",
-      "8": "17 499",
-      "8.5": "12 499",
-      "9": "15 999",
-      "9.5": "11 499",
-      "10": "10 499"
+    "id": "5",
+    "name": "Nike M2K Tekno Cool White (W)",
+    "img": [
+      "https://cdn-img.poizonapp.com/pro-img/origin-img/20230801/0cb75fe8ff6c490c95be5abf3ad0f676.jpg",
+      "https://cdn-img.poizonapp.com/pro-img/origin-img/20230801/d83e8f9ede2f40de973b4febe890669a.jpg",
+      "https://cdn-img.poizonapp.com/pro-img/origin-img/20230801/4032b9e4c1aa4bce85b3f6743a52464d.jpg",
+      "https://cdn-img.poizonapp.com/pro-img/origin-img/20230801/cb966106ba024b0f8db091afb60e5f71.jpg",
+      "https://cdn-img.poizonapp.com/pro-img/origin-img/20230801/a14185ca99a54051bae7082c7b60ecd7.jpg"
+    ],
+    "category": "Nike",
+    "price": "$183",
+    "size": {
+      "5": "$100",
+      "5.5": "$99",
+      "6": "$98",
+      "6.5": "$95",
+      "7": "$95",
+      "7.5": "$94",
+      "8": "$95",
+      "8.5": "$99",
+      "9": "$107",
+      "9.5": "$122",
+      "10": "$129",
+      "10.5": "$143",
+      "11": "$164",
+      "11.5": "$190",
+      "12": "$183"
     }
   },
   {
-    id: "6",
-    name: "Reebok Club C 85",
-    img: "Reebok-Club-C-85.jpg",
-    description:
-      "It's a race, it's a chase, hurry up and feed their face! Who will win? No one knows! Feed the hungry hip-ip-pos!",
-    price: "13 499",
-    size: {
-      "6": "9 999",
-      "6.5": "12 000",
-      "7": "12 499",
-      "7.5": "16 499",
-      "8": "17 499",
-      "8.5": "12 499",
-      "9": "13 499",
-      "9.5": "11 499",
-      "10": "10 499"
+    "id": "6",
+    "name": "Nike M2K Tekno White Pure Platinum",
+    "img": [
+      "https://cdn-img.poizonapp.com/pro-img/origin-img/20220808/1bb669dfebe648cf89c3180b5b02b908.jpg",
+      "https://cdn-img.poizonapp.com/pro-img/origin-img/20220808/8ebb0a7fe6534f41873b354d14656b7d.jpg",
+      "https://cdn-img.poizonapp.com/pro-img/origin-img/20220808/3376b8fe55f84d22a62d326df72c7cfc.jpg",
+      "https://cdn-img.poizonapp.com/pro-img/origin-img/20220808/676ddcf460c2447eb93d6eae505dc98c.jpg",
+      "https://cdn-img.poizonapp.com/pro-img/origin-img/20220808/4dcaec4da2d346f0ba8d3605e96a1108.jpg"
+    ],
+    "category": "Nike",
+    "price": "$85",
+    "size": {
+      "6": "$101",
+      "6.5": "$95",
+      "7": "$84",
+      "7.5": "$84",
+      "8": "$83",
+      "8.5": "$90",
+      "9": "$82",
+      "9.5": "$85",
+      "10": "$79",
+      "10.5": "$84",
+      "11": "$89",
+      "11.5": "$120",
+      "12": "$92"
+    }
+  },
+  {
+    "id": "7",
+    "name": "Nike Fanny pack Unisex",
+    "img": [
+      "https://cdn-img.poizonapp.com/pro-img/origin-img/20220521/fac1c8874dae4284843a783cb2b3598e.jpg",
+      "https://cdn-img.poizonapp.com/pro-img/origin-img/20220521/8a40e4286abb4fa799070b841d706c1a.jpg",
+      "https://cdn-img.poizonapp.com/pro-img/origin-img/20220521/0849a5f9f0d74ff1b54a32a94c0e883d.jpg",
+      "https://cdn-img.poizonapp.com/pro-img/origin-img/20220521/6b3a79c22a3d4057bd31a78e8f669437.jpg"
+    ],
+    "category": "Nike",
+    "price": "$66",
+    "size": {
+      "9": "$66"
+    }
+  },
+  {
+    "id": "8",
+    "name": "Nike Air Force 1 Low 07 Triple Black",
+    "img": [
+      "https://cdn-img.poizonapp.com/pro-img/origin-img/20220212/f66cb369e23c40ea9af4fac595523b49.jpg",
+      "https://cdn-img.poizonapp.com/pro-img/origin-img/20220212/ade1f7c3ff93447dbd96400d742c3285.jpg",
+      "https://cdn-img.poizonapp.com/pro-img/origin-img/20220212/68233dd080de49669c340eb1730e3ad3.jpg",
+      "https://cdn-img.poizonapp.com/pro-img/origin-img/20220212/abbd9f4b28764d85b0630854480e1180.jpg",
+      "https://cdn-img.poizonapp.com/pro-img/origin-img/20220212/237d9a22f2584c6bb980f021e91bc819.jpg"
+    ],
+    "category": "Nike",
+    "price": "$134",
+    "size": {
+      "6": "$135",
+      "6.5": "$134",
+      "7": "$134",
+      "7.5": "$135",
+      "8": "$134",
+      "8.5": "$134",
+      "9": "$134",
+      "9.5": "$135",
+      "10": "$136",
+      "10.5": "$138",
+      "11": "$141",
+      "12": "$140",
+      "12.5": "$253"
     }
   }
 ];
