d5bb84f42c1656dd506a5c47a2ad3703b814b019
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-10-22 18:57:00 +0700

    Update Products.jsx

--- src/Products/Products.jsx
+++ src/Products/Products.jsx
@@ -1,23 +1,51 @@
+import { useState, useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import "./css/item.css";
 import productsData from "./productsData";
 import { Link } from "react-router-dom";
 
 const Products = () => {
-  const products = productsData.map((product) => (
-    <div className="item" key={product.id}> {/* Перенесли ключ сюда */}
-        <Link to={`/products/${product.id}`}>
-      <img src={product.img[0]} alt="" />
-      <h4>{product.price}</h4>
-      <p>{product.name}</p>
-      <button className="add-item">
-          <div className="buy-item">Купить</div>
-      </button>
-        </Link>
-    </div>
-  ));
+  const [items, setItems] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
+  const itemsPerPage = 10;
+  const [startIndex, setStartIndex] = useState(0);
+
+  const loadMore = () => {
+    if (startIndex < productsData.length) {
+      const newItems = productsData.slice(startIndex, startIndex + itemsPerPage);
+      setItems([...items, ...newItems]);
+      setStartIndex(startIndex + itemsPerPage);
+    } else {
+      setHasMore(false);
+    }
+  };
+
+  useEffect(() => {
+    loadMore();
+  }, []);
 
   return (
-    <main>{products}</main>
+    <InfiniteScroll
+      dataLength={items.length}
+      next={loadMore}
+      hasMore={hasMore}
+      loader={<h4>Загрузка товаров...</h4>}
+    >
+      <main>
+      {items.map((product) => (
+        <div className="item" key={product.id}>
+          <Link to={`/products/${product.id}`}>
+            <img src={product.img[0]} alt="" />
+            <h4>{product.price}</h4>
+            <p>{product.name}</p>
+            <button className="add-item">
+              <div className="buy-item">Купить</div>
+            </button>
+          </Link>
+        </div>
+      ))}
+      </main>
+    </InfiniteScroll>
   );
 };
 
