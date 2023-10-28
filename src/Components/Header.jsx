aa9ae05ef84b635e98d39b4c800d8ae242cc6109
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-10-28 21:35:50 +0700

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -16,7 +16,7 @@ function Header({ userId }) {
       <Link to={`/profile/`}>
       <div className="usercard">
         <div className="usercard_block">
-           
+           <p>User ID: {userId}</p>
         <UserProfile userId={userId} />
             {user?.first_name}
             <svg
