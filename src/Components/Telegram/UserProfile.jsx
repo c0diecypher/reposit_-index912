cb45c0a4c0adc88b0be32cf7add1b6e7ced43bdd
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-10-30 21:43:18 +0700

    Update UserProfile.jsx

--- src/Components/Telegram/UserProfile.jsx
+++ src/Components/Telegram/UserProfile.jsx
@@ -10,25 +10,27 @@ function UserProfile({ userId }) {
 
    
   useEffect(() => {
-    // Замените 'userId' на фактический ID пользователя, информацию о котором вы хотите получить
-    // Выполняем GET-запрос на сервер для получения информации о пользователе
-    fetch("https://zipperconnect.space/userProfile/" + userId)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then((data) => {
-        // Обработка успешного ответа
-        setUserData(data);
-        console.log('Received user data:', data);
-      })
-      .catch((err) => {
-        // Обработка ошибки
-        setError(err);
-      });
-  }, [userId]);
+  // Замените 'userId' на фактический ID пользователя, информацию о котором вы хотите получить
+  // Выполняем GET-запрос на сервер для получения информации о пользователе
+  fetch("https://zipperconnect.space/userProfile/" + userId)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then((data) => {
+      // Обработка успешного ответа
+      console.log('Received user data:', data);
+      setUserData(data);
+      setError(null); // Сбросить ошибку, если она существует
+    })
+    .catch((err) => {
+      // Обработка ошибки
+      setError(err);
+      setUserData(null); // Сбросить данные пользователя в случае ошибки
+    });
+}, [userId]);
 
   return (
     <>
