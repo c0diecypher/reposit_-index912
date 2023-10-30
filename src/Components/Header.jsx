780fd29052c1b0fddb2a4f4550640455418d2e46
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-10-30 21:14:28 +0700

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,6 +8,30 @@ import UserProfile from './Telegram/UserProfile';
 
 function Header({ userId }) {
   const { user } = useTelegram();
+  const [userData, setUserData] = useState(null);
+  const [error, setError] = useState(null);
+
+   
+  useEffect(() => {
+    // Замените 'userId' на фактический ID пользователя, информацию о котором вы хотите получить
+    // Выполняем GET-запрос на сервер для получения информации о пользователе
+    fetch(`https://zipperconnect.space/userProfile/${userId}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // Обработка успешного ответа
+        setUserData(data);
+      })
+      .catch((err) => {
+        // Обработка ошибки
+        setError(err);
+      });
+  }, [userId]);
+  
 
   return (
     <>
@@ -16,7 +40,13 @@ function Header({ userId }) {
       <Link to={`/profile/`}>
       <div className="usercard">
         <div className="usercard_block">
-        <UserProfile userId={userId} />
+        {userData ? (
+        <div className="usercard_avatar">
+          <img src={userData.photoUrl} className="usercard_avatar_img" />
+        </div>
+      ) : (
+        <UserProfile />
+          )}
             {user?.first_name}
             <svg
             className="user_block_svg"
