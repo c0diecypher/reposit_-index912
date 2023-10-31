5e6640a6c01e794198701d402c4b40c06bfb89eb
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-10-31 21:15:39 +0700

    Create SettingsProfile.jsx

---
+++ src/Components/Telegram/SettingsProfile.jsx
@@ -0,0 +1,97 @@
+import '../../css/body.css'
+import './css/ProfilePage.css'
+import { useTelegram } from "../Hooks/useTelegram";
+import { useEffect, useState } from 'react';
+import { MainButton } from "@twa-dev/sdk/react" 
+
+
+function SettingsProfile() {
+  const { user } = useTelegram();
+  useEffect(() => {
+    window.scrollTo(0, 0); // Прокрутка вверх при загрузке страницы
+  }, []);
+  const [color] = useState(window.Telegram.WebApp.themeParams.button_color);
+  const [textColor] = useState(window.Telegram.WebApp.themeParams.button_text_color);
+  const [userData, setUserData] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleSaveClick = () => {
+    // Собираем данные из полей ввода
+    const fullName = document.querySelector('.profile-select-input input[name="fullName"]').value;
+    const phoneNumber = document.querySelector('.profile-select-input input[name="phoneNumber"]').value;
+  
+    // Создаем объект с данными, включая userId
+    const newData = {
+      userId,
+      fullName,
+      phoneNumber,
+    };
+  
+    // Отправляем данные на сервер
+    fetch('https://zipperconnect.space/customer/settingsProfile', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newData),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // Обработка успешного ответа
+        setUserData(data);
+        // Опционально: отобразите сообщение об успешном сохранении
+        alert('Данные успешно сохранены!');
+      })
+      .catch((err) => {
+        // Обработка ошибки
+        setError(err);
+      });
+  };
+
+  return (
+    <>
+            <div className="profile-data">
+              <div className='profile-data-title'>
+                  Данные доставки
+              </div>
+                <div className="profile-data-info">
+                  <h2>Данные получателя</h2>
+                </div>
+                <div className="profile-select-info">
+                
+                  <div className="profile-select-input">
+                    <label className="profile-select-label">Фамилия, имя и очетство</label>
+                    <input type="text" className="profile-search-value"/>
+                    <div className="profile-select-info-icon">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="tabler-icon tabler-icon-chevron-right">
+                        <path d="M9 6l6 6l-6 6"></path>
+                      </svg>
+                      </div>
+                  </div>
+                  <div className="profile-select-input">
+                    <label className="profile-select-label">Телефон</label>
+                    <input type="text" className="profile-search-value"/>
+                    <div className="profile-select-info-icon">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="tabler-icon tabler-icon-chevron-right">
+                        <path d="M9 6l6 6l-6 6"></path>
+                      </svg>
+                      </div>
+                  </div>
+                </div>
+             </div>
+              <MainButton 
+                        onClick={handleSaveClick}
+                        text={`Сохранить`}
+                        color={color}
+                        textColor={textColor}
+                        />
+    </>
+  )
+}
+
+export default SettingsProfile;
