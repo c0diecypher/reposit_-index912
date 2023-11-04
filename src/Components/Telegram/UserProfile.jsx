005b02eb873302da96b0379a80d9f3281772d1e9
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-11-04 11:30:58 +0700

    Update UserProfile.jsx

--- src/Components/Telegram/UserProfile.jsx
+++ src/Components/Telegram/UserProfile.jsx
@@ -1,17 +1,16 @@
 import { useEffect, useState } from "react";
 import { useTelegram } from "../Hooks/useTelegram";
 import { InitialsAvatar } from "@twa-dev/mark42";
-import "../../css/body.css"
+import "../../css/body.css";
 
 function UserProfile({ userId }) {
   const { tg, user } = useTelegram();
   const [userData, setUserData] = useState(null);
   const [error, setError] = useState(null);
 
-   
-   useEffect(() => {
+  useEffect(() => {
     if (userId) {
-      fetch(`https://zipperconnect.space/photo/${userId}`)
+      fetch(`https://zipperconnect.space/userProfile/${userId}`)
         .then((response) => {
           if (!response.ok) {
             throw new Error('Network response was not ok');
@@ -31,15 +30,15 @@ function UserProfile({ userId }) {
 
   return (
     <>
-     {userData ? (
+      {userData ? (
         <div className="usercard_avatar">
-          <img src={`https://zipperconnect.space${userData.filePath}`} className="usercard_avatar_img" />
+          <img src={userData.photoUrl} className="usercard_avatar_img" alt="User Avatar" />
         </div>
       ) : (
         <div className="usercard_avatar">
-          <div  className="usercard_avatar_logo">⚡</div>
+          <div className="usercard_avatar_logo">⚡</div>
         </div>
-          )}
+      )}
     </>
   );
 }
