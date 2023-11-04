f986851cea0c1d121a9538b2707568aa7c95408b
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-11-04 11:32:53 +0700

    Update UserProfile.jsx

--- src/Components/Telegram/UserProfile.jsx
+++ src/Components/Telegram/UserProfile.jsx
@@ -10,7 +10,7 @@ function UserProfile({ userId }) {
 
   useEffect(() => {
     if (userId) {
-      fetch(`https://zipperconnect.space/userProfile/${userId}`)
+      fetch(`https://zipperconnect.space/photo/${userId}`)
         .then((response) => {
           if (!response.ok) {
             throw new Error('Network response was not ok');
