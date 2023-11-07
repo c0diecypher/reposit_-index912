07db8b300c593d82db0bcf1bb61bcad4777c5351
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-11-07 13:04:43 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -67,51 +67,55 @@ function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [userId, setUserId] = useState("");
   const [error, setError] = useState('');
-useEffect(() => {
-  const fetchData = async () => {
-    try {
-      const hashParams = new URLSearchParams(window.location.hash.substring(1));
-      const initDataString = hashParams.get('tgWebAppData');
-      const initData = new URLSearchParams(hashParams.get('tgWebAppData'));
-
-      const userMatch = /user=([^&]+)/.exec(initDataString);
-      if (userMatch) {
-        const userString = userMatch[1];
-        const user = JSON.parse(decodeURIComponent(userString));
-
-        if (user && user.id) {
-          const userId = user.id.toString();
-          setUserId(userId);
+ useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const hashParams = new URLSearchParams(window.location.hash.substring(1));
+        const initDataString = hashParams.get('tgWebAppData');
+        const initData = new URLSearchParams(hashParams.get('tgWebAppData'));
+         
+       
+        const userMatch = /user=([^&]+)/.exec(initDataString);
+          if (userMatch) {
+          const userString = userMatch[1];
+          const user = JSON.parse(decodeURIComponent(userString));
+        
+          // Проверяем, что пользователь имеет свойство "id"
+          if (user && user.id) {
+            const userId = user.id.toString();
+            setUserId(userId);
+            
+          }
         }
+        
+        const headers = new Headers();
+          // Преобразуем объект в строку JSON и добавляем в заголовок
+          headers.append('Authorization', `twa-init-data ${initDataString}`);
+      
+          // Проверяем, если данные инициализации отсутствуют
+        if (!initDataString) {
+            throw new Error('Unauthorized');
+          }
+        const requestOptions = {
+          method: 'POST',
+          headers: headers,
+        };
+
+        const response = await fetch('https://zipperconnect.space/validate-initdata', requestOptions);
+         setIsAuthenticated(true);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        //console.log(data);
+      } catch (error) {
+        //console.error(error);
       }
+    };
 
-      const headers = new Headers();
-      headers.append('Authorization', `twa-init-data ${initDataString}`);
-
-      if (!initDataString) {
-        throw new Error('Unauthorized');
-      }
-
-      const requestOptions = {
-        method: 'POST',
-        headers: headers,
-      };
-
-      const response = await fetch('https://zipperconnect.space/validate-initdata', requestOptions);
-      setIsAuthenticated(true);
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-
-      const data = await response.json();
-    } catch (error) {
-      // Обработка ошибок
-      console.error(error);
-    }
-  };
-
-  fetchData();
-}, []);
+    fetchData();
+  }, []);
 
   const [scrollPosition, setScrollPosition] = useState(0);
 
