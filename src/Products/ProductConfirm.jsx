97fafd40ddf0c8b18ecf3189ac7eefa002657ab3
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-11-08 16:23:37 +0700

    Update ProductConfirm.jsx

--- src/Products/ProductConfirm.jsx
+++ src/Products/ProductConfirm.jsx
@@ -45,7 +45,7 @@ function ProductConfirm() {
     .then((data) => {
       if (data && data.paymentUrl) {
         // После получения данных с сервера, перенаправьте текущее окно по полученному URL
-        Telegram.WebApp.openLink = data.paymentUrl;
+        setPaymentUrl(data.paymentUrl);
       } else {
         console.error('Отсутствует ссылка для оплаты.');
       }
