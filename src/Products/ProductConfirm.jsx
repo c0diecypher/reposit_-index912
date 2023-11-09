e6f7998317885293b1ae90d26310c49f65e17a71
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-11-09 13:08:57 +0700

    Update ProductConfirm.jsx

--- src/Products/ProductConfirm.jsx
+++ src/Products/ProductConfirm.jsx
@@ -45,8 +45,9 @@ function ProductConfirm() {
   })
     .then((response) => response.json())
     .then((data) => {
-      if (data.paymentUrl) {
+      if (data.paymentUrl && data.getPaymentStatus) {
           Telegram.WebApp.openLink(data.paymentUrl);
+          setStatus(data.getPaymentStatus);
         } else {
         console.error('Отсутствует ссылка для оплаты.');
       }
@@ -62,23 +63,142 @@ useEffect(() => {
   }
 }, [paymentLink]);
 
+  return (
+    <>
+    <div className="confirm-item" key={productId}>
+      <div className="images-slider-wrapper">
+        <div className="images-slider-images">
+          <div className="images-slider-image-item">
+            <div className="image-item-wrapper">
+              <img src={productData.img[0]} alt="photo" />
+            </div>
+          </div>
+          <div className="images-slider-image-item">
+            <div className="image-item-wrapper">
+              <img src={productData.img[1]} alt="photo" />
+            </div>
+          </div>
+          <div className="images-slider-image-item">
+            <div className="image-item-wrapper">
+              <img src={productData.img[2]} alt="photo" />
+            </div>
+          </div>
+          <div className="images-slider-image-item">
+            <div className="image-item-wrapper">
+              <img src={productData.img[3]} alt="photo" />
+            </div>
+          </div>
+          <div className="images-slider-image-item">
+            <div className="image-item-wrapper">
+              <img src={productData.img[4]} alt="photo" />
+            </div>
+          </div>
+          <div className="images-slider-image-item">
+            <div className="image-item-wrapper">
+              <img src={productData.img[5]} alt="photo" />
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <div className="bg-full-item-name">
+    <div className="confirm-item-name">{productData.name}
+        <span className="confirm-item-size" > размер {size} US</span>
+      </div>
+      </div>
+      <div className="item-order-info">
+      <div className="confirm-item-price">
+         {price}₽
+      </div>
+      <div className="public-oferta">
+        {status && <p>Статус платежа: {status}</p>}
+        <p className="public-ofert-text">Оплачивая заказ, вы соглашаетесь <br/>с условиями <a className="public-oferta-link">публичной оферты</a></p>
+      </div>
+       <hr/>
+      </div>
+  </div>
+  <div className="help-ful">
+    <h2 className="help-title">Полезная информация</h2>
+    <div className="help-stories">
+    <Stories />
+    </div>
+  </div>
+  <MainButton 
+    onClick={() => {
+    onSendData();
+  }}
+                        color={color}
+                        textColor={textColor}
+                        text={`Купить за ${price}`}
+                        />
+  </>
+  );
+}
+
+export default ProductConfirm;import "./css/Product.css";
+import { useState, useEffect, useCallback } from "react"
+import { useParams, useLocation, useNavigate } from "react-router-dom";
+import Stories from "../Stories/Stories"
+import { useTelegram } from "../Components/Hooks/useTelegram"
+import { MainButton } from "@twa-dev/sdk/react"
+
+function ProductConfirm() {
   useEffect(() => {
-    const eventSource = new EventSource('https://zipperconnect.space/customer/settings/client/buy/offer/pay'); // Указывайте правильный URL SSE-соединения
-
-    eventSource.onmessage = (event) => {
-      const data = JSON.parse(event.data);
-      setStatus(data.paymentStatus);
-    };
-
-    eventSource.onerror = (error) => {
-      console.error('Ошибка SSE:', error);
-    };
-
-    // Очистка SSE-соединения при размонтировании компонента
-    return () => {
-      eventSource.close();
-    };
+    window.scrollTo(0, 0); // Прокрутка вверх при загрузке страницы
   }, []);
+  const { productId, size, price, name, img } = useParams();
+  const location = useLocation();
+  
+
+  // Декодируйте JSON-строку и преобразуйте ее в объект с данными о товаре
+  const { productData } = location.state || {};
+
+  // Отображаем информацию о товаре
+
+  const [color] = useState(window.Telegram.WebApp.themeParams.button_color);
+  const [textColor] = useState(
+    window.Telegram.WebApp.themeParams.button_text_color
+  );
+  const navigate = useNavigate();
+  const [paymentLink, setPaymentLink] = useState('');
+  const {queryId, userId} = useTelegram();
+  const [status, setStatus] = useState('');
+  const onSendData = () => {
+  const data = {
+    name: productData.name,
+    price: productData.price,
+    size: productData.size,
+    queryId,
+    userId,
+    order_id: productData.order_id,
+  };
+
+  fetch('https://zipperconnect.space/customer/settings/client/buy/offer/pay', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.paymentUrl && data.sendPaymentRequest) {
+          Telegram.WebApp.openLink(data.paymentUrl);
+          setStatus(data.sendPaymentRequest);
+        } else {
+        console.error('Отсутствует ссылка для оплаты.');
+      }
+    })
+    .catch((error) => {
+      console.error('Ошибка отправки данных на сервер:', error);
+    });
+};
+
+useEffect(() => {
+  if (paymentLink) {
+    Telegram.WebApp.openLink(paymentLink, { try_instant_view: true })
+  }
+}, [paymentLink]);
 
   return (
     <>
