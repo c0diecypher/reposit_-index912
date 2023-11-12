83d2dd3bcf20d7082877e42723070aa5b3c7a060
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-11-12 12:37:14 +0700

    Update ProductConfirm.jsx

--- src/Products/ProductConfirm.jsx
+++ src/Products/ProductConfirm.jsx
@@ -135,11 +135,14 @@ const checkPaymentStatus = async () => {
     <div className="item-order-info">
       <div className="status-selection-steps-box">
         <div className="status-selection-step">
-            <div className="status-selection-step-inner" style={{ color: 
-                paymentStatus === 'Отмена' ? '#b54531' :
-                paymentStatus === 'Оплачен' ? '#31b545' :
-                paymentStatus === 'Ожидается оплата' ? 'var(--tg-theme-hint-color)' :
-                'initial' /* или любой другой цвет по умолчанию */
+            <div className="status-selection-step-inner" style={{ 
+                сolor: paymentStatus === 'Отмена' ? '#b54531' :
+                       paymentStatus === 'Оплачен' ? '#31b545' :
+                       paymentStatus === 'Ожидается оплата' ? 'var(--tg-theme-hint-color)',
+                                  
+                border: paymentStatus === 'Отмена' ? '1px solid #b54531' :
+                        paymentStatus === 'Оплачен' ? '1px solid #31b545' :
+                        paymentStatus === 'Ожидается оплата' ? '1px solid var(--tg-theme-hint-color)'
               }}>
               <svg viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="m4.917 10 4.166 4.166 8.334-8.333" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path></svg>
             </div>
