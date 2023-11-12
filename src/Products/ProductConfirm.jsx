cccf144cea0f4e55d75899381c9a617ae2aa4202
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-11-12 12:08:50 +0700

    Update ProductConfirm.jsx

--- src/Products/ProductConfirm.jsx
+++ src/Products/ProductConfirm.jsx
@@ -134,7 +134,7 @@ const checkPaymentStatus = async () => {
     </div>
     <div className="item-order-info">
       <div className="status-selection-steps-box">
-        <div className="status-selection-step">
+        <div className={`status-selection-step ${paymentStatus === 'Оплачен' ? 'paid' : (paymentStatus === 'Ожидается оплата' ? 'wait' : '')}`}>
             <div className="status-selection-step-inner">
               <svg viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="m4.917 10 4.166 4.166 8.334-8.333" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path></svg>
             </div>
