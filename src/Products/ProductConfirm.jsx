ecf231be3ca82770883747d31fb3b33fea62987b
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-11-20 12:36:43 +0700

    Update ProductConfirm.jsx

--- src/Products/ProductConfirm.jsx
+++ src/Products/ProductConfirm.jsx
@@ -163,15 +163,30 @@ function ProductConfirm() {
               <svg viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="m4.917 10 4.166 4.166 8.334-8.333" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path></svg>
             </div>
             <span className="status-selection-line"></span>
-            <div className="status-selection-step-inner">
+            <div className="status-selection-step-inner" style={{ color: 
+                paymentData === 'SENT' ? '#31b545' :
+                paymentData === 'CANCEL' ? '#b54531' :
+                paymentData === 'WAIT' ? 'var(--tg-theme-hint-color)' :
+                'var(--tg-theme-hint-color)' /* или любой другой цвет по умолчанию */
+              }}>
               <svg viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M33.333 12.5 20 5 6.667 12.5m26.666 0v15L20 35m13.333-22.5L20 20m0 15L6.667 27.5v-15M20 35V20M6.667 12.5 20 20m6.667-11.25-13.334 7.5" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path></svg>
             </div>
             <span className="status-selection-line"></span>
-            <div className="status-selection-step-inner">
+            <div className="status-selection-step-inner" style={{ color: 
+                paymentData === 'TRANSIT' ? '#31b545' :
+                paymentData === 'CANCEL' ? '#b54531' :
+                paymentData === 'WAIT' ? 'var(--tg-theme-hint-color)' :
+                'var(--tg-theme-hint-color)' /* или любой другой цвет по умолчанию */
+              }}>
               <svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.5 14.166a1.667 1.667 0 1 1-3.333 0m3.333 0a1.667 1.667 0 1 0-3.333 0m3.333 0h5m-8.333 0H2.5v-3.333m13.333 3.333a1.667 1.667 0 1 1-3.333 0m3.333 0a1.667 1.667 0 1 0-3.333 0m3.333 0H17.5v-5m-15.833-5h9.166v10m6.667-5h-6.667m6.667 0L15 5h-4.167M2.5 7.5h3.333" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path></svg>
             </div>
             <span className="status-selection-line"></span>
-            <div className="status-selection-step-inner">
+            <div className="status-selection-step-inner" style={{ color: 
+                paymentData === 'DELIVERED' ? '#31b545' :
+                paymentData === 'CANCEL' ? '#b54531' :
+                paymentData === 'WAIT' ? 'var(--tg-theme-hint-color)' :
+                'var(--tg-theme-hint-color)' /* или любой другой цвет по умолчанию */
+              }}>
               <svg viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.333 35V8.333a8.333 8.333 0 0 1 11.667 0 8.333 8.333 0 0 0 11.667 0v15a8.333 8.333 0 0 1-11.667 0 8.333 8.333 0 0 0-11.667 0" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path></svg>
             </div>         
         </div>
