7b2944a5473d8c5637c1d41617dabace07d72fba
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-11-23 12:33:24 +0700

    Update ButtonBonus.jsx

--- src/Components/ButtonBonus.jsx
+++ src/Components/ButtonBonus.jsx
@@ -9,16 +9,6 @@ function ButtonBonus() {
       <div className='action-buttons'>
         <div className='action-card'>
             <div className='action-card-QWE13S'>
-            <svg xmlns="http://www.w3.org/2000/svg">
-  <defs>
-    <linearGradient id="animatedGradient" x1="0%" y1="0%" x2="100%" y2="0%">
-      <stop offset="0%" style={{ stopColor: '#f79533' }} />
-      <stop offset="25%" style={{ stopColor: '#ef4e7b' }} />
-      <stop offset="50%" style={{ stopColor: '#5073b8' }} />
-      <stop offset="75%" style={{ stopColor: '#07b39b' }} />
-    </linearGradient>
-  </defs>
-</svg>
                 <div className='action-card-bg'>
                     <div className="action-inner">
                         <div className="action-head">
