8deeb6f3fe9d0c884f8b5ffc7015205cc2112f8c
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-12-03 20:53:58 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
-import App from './App.jsx';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import { BrowserRouter as Router } from "react-router-dom";
 
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
       <Router>
         <App />
       </Router>
-    </QueryClientProvider>
-    <ReactQueryDevtools />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
