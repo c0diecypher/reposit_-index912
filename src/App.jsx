bebc983387367a57f3bb5b7d6608496eb165ad05
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-12-03 21:22:57 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -142,27 +142,29 @@ function App() {
         exact
         path="/"
         element={
-          <>
-            {Telegram.WebApp.setHeaderColor('secondary_bg_color')}
-            {tg.expand()}
-            {tg.enableClosingConfirmation()}
-            {shouldShowBanner ? (
-            <Header userId={userId} />
-            <Searchbar />
-            <Stories />
-              {isAuthenticated && <ButtonBonus userId={userId} />}
-              {shouldShowBanner && <Banner userId={userId}/>}
-              {isAuthenticated && <BasketItem userId={userId} />}
-              <div></div>
-              {isAuthenticated && <BasketPaid userId={userId} />}
-            <Catalog  userId={userId} />
-            {isAuthenticated && <Products  userId={userId} />}
-            <Footer />
-            ) : (
+            <>
+              {Telegram.WebApp.setHeaderColor('secondary_bg_color')}
+              {tg.expand()}
+              {tg.enableClosingConfirmation()}
+              {shouldShowBanner ? (
+                <>
+                  <Header userId={userId} />
+                  <Searchbar />
+                  <Stories />
+                  {isAuthenticated && <ButtonBonus userId={userId} />}
+                  {shouldShowBanner && <Banner userId={userId} />}
+                  {isAuthenticated && <BasketItem userId={userId} />}
+                  <div></div>
+                  {isAuthenticated && <BasketPaid userId={userId} />}
+                  <Catalog userId={userId} />
+                  {isAuthenticated && <Products userId={userId} />}
+                  <Footer />
+                </>
+              ) : (
                 <div>У вас нет доступа:(</div>
               )}
-          </>
-        }
+            </>
+          }
         >
 
         </Route>
