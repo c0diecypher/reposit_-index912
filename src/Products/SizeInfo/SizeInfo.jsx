394b527639d1fee7e6bf06d6f7c932de09381449
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-12-09 15:22:10 +0700

    Update SizeInfo.jsx

--- src/Products/SizeInfo/SizeInfo.jsx
+++ src/Products/SizeInfo/SizeInfo.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 function SizeInfo() {
   return (
     <div className="size">
-      <p>Размер US </p>
+      <p>Размер (EU) </p>
       <Link to={`/products/size/`}>
         <a>Размерная сетка</a>
       </Link>
