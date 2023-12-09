5da42b7245c8b3ec5c3c2c903d999615bb624bec
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-12-09 15:37:44 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { useTelegram } from "./Components/Hooks/useTelegram";
 import { BrowserRouter as Router } from "react-router-dom";
 
+const { tg } = useTelegram();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <Router>
+        {Telegram.WebApp.setHeaderColor('secondary_bg_color')}
+        {tg.expand()}
+        {tg.enableClosingConfirmation()}
         <App />
       </Router>
   </React.StrictMode>,
