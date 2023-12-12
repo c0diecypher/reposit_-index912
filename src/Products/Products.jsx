f17b365a6f620e81e9219cb0fcefe27c4974e7af
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-12-13 00:38:03 +0700

    Update Products.jsx

--- src/Products/Products.jsx
+++ src/Products/Products.jsx
@@ -5,24 +5,8 @@ import productsData from "./productsData";
 import { Link } from "react-router-dom";
 
 const Products = () => {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(productsData);
   const [hasMore, setHasMore] = useState(true);
-  const itemsPerPage = 10;
-  const [startIndex, setStartIndex] = useState(0);
-
-  const loadMore = () => {
-    if (startIndex < productsData.length) {
-      const newItems = productsData.slice(startIndex, startIndex + itemsPerPage);
-      setItems([...items, ...newItems]);
-      setStartIndex(startIndex + itemsPerPage);
-    } else {
-      setHasMore(false);
-    }
-  };
-
-  useEffect(() => {
-    loadMore();
-  }, []);
 
   useEffect(() => {
     if (items.length) {
@@ -33,31 +17,37 @@ const Products = () => {
       }
     }
   }, [items]);
-  
 
-    const products = productsData.map((product) => (
-      <div className="item" key={product.id}>
-          <Link to={`/products/${product.id}`}>
-          <div className="item-img">
-              <img src={product.img[0]} alt="" />
-            </div>
-            <div className="item-info">
-              <h4>{product.price}₽</h4>
-              <p>{product.name}</p>
-              <button className="add-item">
-                <div className="buy-item">Купить</div>
-              </button>
-              </div>
-          </Link>
-      </div>
-    ));
-  
-    return (
+  const products = items.map((product) => (
+    <div className="item" key={product.id}>
+      <Link 
+        to={`/products/${product.id}`} 
+        onClick={() => sessionStorage.setItem('scrollPosition', window.scrollY)}
+      >
+        <div className="item-img">
+          <img src={product.img[0]} alt="" />
+        </div>
+        <div className="item-info">
+          <h4>{product.price}₽</h4>
+          <p>{product.name}</p>
+          <button className="add-item">
+            <div className="buy-item">Купить</div>
+          </button>
+        </div>
+      </Link>
+    </div>
+  ));
+
+  return (
+    <InfiniteScroll
+      dataLength={items.length}
+      next={setItems}
+      hasMore={hasMore}
+      loader={<p>...</p>}
+    >
       <main>{products}</main>
-    );
-
-  
-
+    </InfiniteScroll>
+  );
 };
 
 export default Products;
