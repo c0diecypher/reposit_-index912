2922211919397b79830f0c00b842a5b7491a4e1b
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-12-15 15:25:51 +0700

    Create AppRouter.jsx

---
+++ src/AppRouter.jsx
@@ -0,0 +1,31 @@
+
+
+import { Route, Navigate, Routes } from "react-router-dom";
+import { routes } from './router'
+
+const AppRouter = ({ cart, addToCart, sendDataToParent, dataFromMainButton, handleDataFromMainButton }) => {
+    return (
+      <Routes>
+        {routes.map(route => (
+          <Route 
+            element={<route.component 
+                cart={cart} 
+                addToCart={addToCart} 
+                sendDataToParent={sendDataToParent} 
+                dataFromMainButton={dataFromMainButton} 
+                handleDataFromMainButton={handleDataFromMainButton} 
+            />}
+            path={route.path} 
+            exact={route.exact}
+            key={route.path} 
+          />
+        ))}
+        <Route
+          path="*"
+          element={<Navigate to="/" replace />}
+        />
+      </Routes>
+    );
+  }
+  
+  export default AppRouter;
