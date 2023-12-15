370e8ba7982b714bbee947245a8d434cf8a017be
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-12-15 15:26:12 +0700

    Create router.jsx

---
+++ src/router.jsx
@@ -0,0 +1,27 @@
+import Layout from "./Layout";
+import BonusPage from "./Components/BonusPage";
+import ProfilePage from "./Components/Telegram/ProfilePage";
+import SettingsProfile from "./Components/Telegram/SettingsProfile";
+import ProductDetail from "./Products/ProductDetail";
+import ProductConfirm from "./Products/ProductConfirm";
+import ProductPay from "./Products/ProductPay";
+import Search from "./Search/Search";
+import SizeInfoDetails from "./Products/SizeInfo/SizeInfoDetails";
+import FilterProducts from "./Products/FilterProducts";
+import FilterResults from "./Products/FilterResults";
+import ModalWindow from "./ModalWindow";
+
+export const routes = [
+    {path:'/', component: Layout, exact:true},
+    {path:'/bonus', component: BonusPage, exact:true},
+    {path:'/profile/', component: ProfilePage, exact:true},
+    {path:'/profile/settings', component: SettingsProfile, exact:true},
+    {path:'/products/:productId', component: ModalWindow, exact:true},
+    {path:'/products/confirm/:name/:size/:price', component: ProductConfirm, exact:true},
+    {path:'/products/confirm/offer/:name/:size/:price', component: ProductPay, exact:true},
+    {path:'/search', component: Search, exact:true},
+    {path:'/products/size/', component: SizeInfoDetails, exact:true},
+    {path:'/filtered', component: FilterProducts, exact:true},
+    {path:'/filtered-results', component: FilterResults, exact:true}
+
+]
