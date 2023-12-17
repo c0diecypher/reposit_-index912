0e7b61bd1093b070286a5a014b97aa856e79c5c9
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-12-17 17:32:39 +0700

    Update HomeBackButton.jsx

--- src/Products/HomeBackButton.jsx
+++ src/Products/HomeBackButton.jsx
@@ -2,7 +2,6 @@ import { BackButton } from "@twa-dev/sdk/react";
 
 const HomeBackButton = ({ closeModal, ...props }) => {
   function handleClick() {
-    console.log('Нажата кнопка назад!');
     closeModal();  
     console.log('Нажата closeModal', closeConfirm);
     props.onClick && props.onClick();
