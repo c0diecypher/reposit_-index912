5347c0dde36892da9024df0d7b9e415d98a3b585
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-12-18 00:49:28 +0700

    Update ModalWindow.jsx

--- src/ModalWindow.jsx
+++ src/ModalWindow.jsx
@@ -262,7 +262,6 @@ return (
       </div>
           {openConfirm && (
         <>
-      <BackButton onClick={(closeConfirm)} />
       <Confirm active={openConfirm} setActive={setOpenConfirm} closeConfirm={closeConfirm} product={paymentData} closeModal={closeModal} />
       </>
       )}
