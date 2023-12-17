c57bf0012d330ccd6ab545e76f827e805ea87631
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-12-17 18:54:59 +0700

    Update HomeBackButton.jsx

--- src/Products/HomeBackButton.jsx
+++ src/Products/HomeBackButton.jsx
@@ -1,13 +1,13 @@
 import { BackButton } from "@twa-dev/sdk/react"; 
 
 const HomeBackButton = ({ closeModal, ...props }) => {
-  function handleClick() {
+  const handleClose = () => {
     closeModal();  
     console.log('Нажата closeModal', closeModal);
     props.onClick && props.onClick();
   };
 
-  return <BackButton {...props} onClick={handleClick} />;
+  return <BackButton {...props} onClick={handleClose} />;
 };
 
 export default HomeBackButton;
