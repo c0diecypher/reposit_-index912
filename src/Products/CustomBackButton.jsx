28c032ba08c964e550b9d663d3fd6a2215b11dd9
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-12-17 19:24:06 +0700

    Update CustomBackButton.jsx

--- src/Products/CustomBackButton.jsx
+++ src/Products/CustomBackButton.jsx
@@ -1,19 +1,18 @@
-import { BackButton } from "@twa-dev/sdk/react"; 
+import { BackButton } from "@twa-dev/sdk/react";
 
 const CustomBackButton = ({ closeConfirm, closeModal, onClick, ...props }) => {
-
   const handleClick = () => {
     console.log('Нажата кнопка назад!');
     if (closeConfirm) {
       closeConfirm();
       console.log('Нажата closeConfirm', closeConfirm);
-    } else {
+    } else if (closeModal) {
       closeModal();
       console.log('Нажата closeModal', closeModal);
     }
   };
 
-  return <BackButton onClick={handleClick} />;
+  return <BackButton onClick={handleClick} {...props} />;
 };
 
 export default CustomBackButton;
