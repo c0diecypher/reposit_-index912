e991712103300f3becc0ef074ec6b5b87d03e13c
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-12-18 13:57:36 +0700

    Update Layout.jsx

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -11,60 +11,30 @@ import Products from "./Products/Products";
 import { useState, useEffect, useCallback } from "react";
 import HomeBackButton from './Products/HomeBackButton'
 import { BackButton } from "@twa-dev/sdk/react"; 
-import { useNavigate } from 'react-router-dom';
 
 function Layout({cart, onDataUpdate, dataFromMainButton, userId}) {
     const [modalActive, setModalActive] = useState(false);
     const [modalProductId, setModalProductId] = useState(null);
-
     const openModal = (product) => { 
       setModalProductId(product);
       // Изменяем маршрут, чтобы открыть модальное окно с определенным productId
-      
+      window.history.pushState(null, `/`, `/products/${product.id}`);
       // Устанавливаем флаг modalActive в true
       setModalActive(true);
-      document.body.classList.add("product-detail");
+        document.body.classList.add("product-detail");
     };
 
-    const closeModal = useCallback(() => {
-      setModalActive(false);
+    const closeModal = () => {
+        setModalActive(false);
       setModalProductId(null);
       // Заменяем текущий URL измененным URL
       document.body.classList.remove("product-detail");
-    }, [setModalActive, setModalProductId]);
-
-    // Effect to show/hide the BackButton based on modalActive state
-    useEffect(() => {
-  const backButton = Telegram.WebApp.BackButton;
-
-  if (modalActive) {
-    console.log('Showing back button');
-    backButton.show().onClick(() => {
-      console.log('Back button clicked');
-      closeModal();
-    });
-  } else {
-    console.log('Hiding back button');
-    backButton.hide().offClick(() => {
-      console.log('Back button click handler removed');
-      closeModal();
-    });
-  }
-
-  return () => {
-    console.log('Cleaning up back button');
-    backButton.hide().offClick(() => {
-      console.log('Back button click handler removed during cleanup');
-      closeModal(); // Вызываем closeModal при размонтировании компонента
-    });
-  };
-
-}, [modalActive, closeModal]);
-const navigate = useNavigate();
-    return (
-      <>
-        {modalActive && (<>
-          
+    };
+  
+  return (
+    <>
+      {modalProductId && (<>
+          <BackButton onClick={(closeModal)} />
           <ModalWindow 
             active={modalActive} 
             setActive={setModalActive} 
@@ -72,29 +42,33 @@ const navigate = useNavigate();
             product={modalProductId} 
             onDataUpdate={onDataUpdate}
             dataFromMainButton={dataFromMainButton}
-            navigate={navigate}
-          />
-        </>)}
-        <Header userId={userId}/>
-        <Searchbar userId={userId}/>
-        <Stories userId={userId} />
-        <ButtonBonus userId={userId} />
-        <Banner userId={userId}/>
-        {cart.length > 0 && (
-          <BasketItem 
-            cart={cart}
-            userId={userId}
-          />
-        )}
-        <Catalog userId={userId} />
-        <Products 
-          setModalActive={setModalActive} 
-          openModal={openModal} 
-          userId={userId}
-        />
-        <Footer />
-      </>
-    );
+            />
+          </>)}
+         <Header userId={userId}/>
+                <Searchbar userId={userId}/>
+                <Stories userId={userId} />
+                <ButtonBonus userId={userId} />
+                <Banner userId={userId}/>
+                {cart.length > 0 && (
+                <BasketItem 
+                cart={cart}
+                userId={userId}
+                />
+                )}
+                <Catalog userId={userId} />
+
+                <Products 
+                setModalActive={setModalActive} 
+                openModal={openModal} 
+                userId={userId}/>
+                
+                
+            <Footer />
+            
+            
+
+    </>
+  )
 }
 
 export default Layout;
