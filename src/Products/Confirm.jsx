655c4f9cbfc36eb203b6bd1dd16f539b8c1aa106
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-12-18 13:00:13 +0700

    Update Confirm.jsx

--- src/Products/Confirm.jsx
+++ src/Products/Confirm.jsx
@@ -1,18 +1,20 @@
-
-import '../css/modal.css';
-import CustomBackButton from './CustomBackButton'
-import { BackButton, MainButton } from "@twa-dev/sdk/react" 
 import { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom';
 
 function Confirm({ active, setActive, product, closeConfirm, closeModal, openConfirm }) {
+  const navigate = useNavigate();
+
   useEffect(() => {
+    const handleBackButtonClick = () => {
+      console.log('Back button clicked in Confirm');
+      setActive(false);
+      navigate(-1); // Вернуться на предыдущий маршрут
+    };
+
     const backButton = Telegram.WebApp.BackButton;
 
     if (active) {
-      backButton.show().onClick(() => {
-        console.log('Back button clicked in Confirm');
-        setActive(false);
-      });
+      backButton.show().onClick(handleBackButtonClick);
     } else {
       backButton.hide().offClick(() => {
         console.log('Back button click handler removed in Confirm');
@@ -26,7 +28,8 @@ function Confirm({ active, setActive, product, closeConfirm, closeModal, openCon
         setActive(false);
       });
     };
-  }, [active, setActive, closeConfirm, product]);
+
+  }, [active, setActive, navigate]);
 
   if (!active || !product) {
     console.error('Confirm component not rendered. Active:', active, 'Product:', product);
@@ -40,32 +43,33 @@ function Confirm({ active, setActive, product, closeConfirm, closeModal, openCon
   const [text] = useState(`Купить за ${product.price}`);
   const Payment = () => {
     console.log('payment click');
-  }
+  };
+
   return (
     <>
-     {active && (
-    <> 
-    <div className={active ? 'confirm active' : 'confirm'}>
-      <div className="confirm__content" onClick={(e) => e.stopPropagation()}>
-        <p>{product.id}</p>
-        <p>{product.name}</p>
-        <p>{product.order_id}</p>
-        <p>{product.size}</p>
-        <p>{product.price}</p>
-        <p>{product.img}</p>
-      </div>
-    </div>
       {active && (
-       <MainButton 
-      onClick={Payment}
-      text={text}
-      color={color}
-      textColor={textColor}
-      />
+        <>
+          <div className={active ? 'confirm active' : 'confirm'}>
+            <div className="confirm__content" onClick={(e) => e.stopPropagation()}>
+              <p>{product.id}</p>
+              <p>{product.name}</p>
+              <p>{product.order_id}</p>
+              <p>{product.size}</p>
+              <p>{product.price}</p>
+              <p>{product.img}</p>
+            </div>
+          </div>
+          {active && (
+            <MainButton 
+              onClick={Payment}
+              text={text}
+              color={color}
+              textColor={textColor}
+            />
+          )}
+        </>
       )}
-      </>
-    )}
-  </>
+    </>
   );
 }
 
