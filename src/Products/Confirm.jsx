36accf53afc01a3d9d808fb18f76a7faccd48c0b
c0diecypher <53970420+c0diecypher@users.noreply.github.com>
2023-12-18 13:08:53 +0700

    Update Confirm.jsx

--- src/Products/Confirm.jsx
+++ src/Products/Confirm.jsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from 'react-router-dom';
 import { MainButton } from "@twa-dev/sdk/react" 
 function Confirm({ active, setActive, product, closeConfirm, closeModal, openConfirm, navigate }) {
-  const navigate = useNavigate();
 
   useEffect(() => {
     const handleBackButtonClick = () => {
